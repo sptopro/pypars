9f1809e119a2b127745ba341466567d86c59504c
septo <septopro@gmail.com>
2019-12-03 21:30:36 +0700

    tst

--- get_pages.py
+++ get_pages.py
@@ -1,25 +1,34 @@
+import json
+
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
-def drug_data():
-    url = 'https://auto.ru/cars/audi/a8/2305474/all/?sort=fresh_relevance_1-desc&output_type=list&'
+headers = {'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                         'AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/78.0.3904.108 Safari/537.36'}
 
-    while url:
-        print(url)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text ,"lxml")
+url = "https://auto.ru/cars/audi/a8/2305474/all/?sort=fresh_relevance_1-desc&output_type=list&0"
 
-        #data = soup.select('name-head a')
-        #for link in data:
-        #    href = 'https://www.drugbank.ca/drugs/' + link.get('href')
-        #    pages_data(href)
 
-        # next page url
-        url = soup.findAll('a', {'class': 'page-link', 'rel': 'next'})
-        print(url)
-        if url:
-            url = 'https://www.drugbank.cahttps://auto.ru/cars/audi/a8/2305474/all/?sort=fresh_relevance_1-desc&output_type=list&' + url[0].get('href')
-        else:
-            break
+#page = requests.get(url, headers=headers)
+def getLinks(url):
+    global links
+    links = []
+    page = requests.get(url)
+    data = page.text
+    soup = bs(data)
+    for link in soup.find_all('a', {"class": "Button Button_color_whiteHoverBlue Button_size_s Button_type_link Button_"
+                                             "width_default ListingPagination-module__page"}):
+        links.append(link.get('href'))
+    return links
 
-drug_data()
+
+print(getLinks(url))
+
+page = requests.get(url, headers=headers)
+soup = bs(page.content, 'html.parser')
+
+for i in getLinks(url):
+    result = [soup.find_all("div", {"class": "ListingItemPrice-module__content"})]
+    print(result)
--- prs.py
+++ prs.py
@@ -1,5 +1,6 @@
+import re
 import time
-
+import urllib3
 import requests
 from bs4 import BeautifulSoup as bs
 
@@ -8,11 +9,16 @@ headers = {'accept': '*/*',
                          'AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/78.0.3904.108 Safari/537.36'}
 
-url = 'https://auto.ru/cars/audi/a8/2305474/all/?sort=fresh_relevance_1-desc&output_type=list&page=1'
+url = "https://auto.ru/cars/audi/a8/2305474/all/?sort=fresh_relevance_1-desc&output_type=list&page=1"
 page = requests.get(url, headers=headers)
 soup = bs(page.content, 'html.parser')
-url_1 = soup.findAll('a', {'class': 'page-link', 'rel': 'next'})
-print(url_1)
+http = urllib3.PoolManager()
+response = http.request('GET', url)
+s_link = bs(response.data)
+for link in s_link.findAll('a', aattrs={'href': re.compile("^http://")}):
+    print(link.get('href'))
+#for links in soup.find_all("div", {"class": "ListingCarsPagination-module__container"}):
+#    print(links)
 
-#for each_div in soup.findAll("div", {"class": "ListingItemPrice-module__content"}):
+#for each_div in soup.find_all("div", {"class": "ListingItemPrice-module__content"}):
 #    print(each_div)
